2ecdc3dda77366b2ed64edc1cd9dc98ee0dcf751
anvitrola <vitoriavianathiss@outlook.com>
2021-05-31 20:40:01 -0300

    isolating api fetch

--- src/services/drinkServices.js
+++ src/services/drinkServices.js
@@ -1,3 +1,5 @@
+import getIngredients from "../utils/GetIngredients";
+
 export const GetDrinks = async () => {
   try {
     const response = await fetch(
@@ -16,10 +18,36 @@ export const GetDrinks = async () => {
     });
 
     return apiDrinks;
-  } 
-  catch (err) {
+  } catch (err) {
     console.log(err);
   }
 };
 
-export const GetDrink = (id) => {};
+export const GetDrink = async (id) => {
+  try {
+    const response = await fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+    );
+
+    if (!response.ok) throw new Error("Request error!");
+
+    const data = (await response.json()).drinks[0];
+
+    console.log(data);
+
+    const apiDrink = {
+      name: data.strDrink,
+      thumbnail: data.strDrinkThumb,
+      id: data.idDrink,
+      alcoolhic: data.strAlcoholic,
+      glass: data.strGlass,
+      recipe: data.strInstructions,
+      category: data.strCategory,
+      ingredients: getIngredients(data),
+    };
+
+    return apiDrink;
+  } catch (err) {
+    console.log(err);
+  }
+};
