000bc82e82ad3cefc5b8791de01aca072bfcb4c1
anvitrola <vitoriavianathiss@outlook.com>
2021-06-01 20:27:46 -0300

    adding search and filtered fetch

--- src/services/drinkServices.js
+++ src/services/drinkServices.js
@@ -33,8 +33,6 @@ export const GetDrink = async (id) => {
 
     const data = (await response.json()).drinks[0];
 
-    console.log(data);
-
     const apiDrink = {
       name: data.strDrink,
       thumbnail: data.strDrinkThumb,
@@ -47,6 +45,54 @@ export const GetDrink = async (id) => {
     };
 
     return apiDrink;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const SearchDrink = async (drinkName) => {
+  try {
+    const response = await fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkName}`
+    );
+
+    if (!response.ok) throw new Error("Request error!");
+
+    const data = (await response.json()).drinks;
+
+    const apiDrinks = data.map(({ strDrink, strDrinkThumb, idDrink }) => {
+      return {
+        name: strDrink,
+        thumbnail: strDrinkThumb,
+        id: idDrink,
+      };
+    });
+
+    return apiDrinks;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const GetFilteredDrinks = async (filter) => {
+  try {
+    const response = await fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=${filter}`
+    );
+
+    if (!response.ok) throw new Error("Request error!");
+
+    const data = (await response.json()).drinks;
+
+    const apiDrinks = data.map(({ strDrink, strDrinkThumb, idDrink }) => {
+      return {
+        name: strDrink,
+        thumbnail: strDrinkThumb,
+        id: idDrink,
+      };
+    });
+
+    return apiDrinks;
   } catch (err) {
     console.log(err);
   }
