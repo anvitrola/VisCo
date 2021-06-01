4f01ba8fedb5bd1dc5a75eccecb240bb3ed2af74
anvitrola <vitoriavianathiss@outlook.com>
2021-06-01 20:30:21 -0300

    drink search feature

--- src/pages/SearchDrinks/SearchDrinks.js
+++ src/pages/SearchDrinks/SearchDrinks.js
@@ -1,11 +1,66 @@
-import { Container } from './SearchDrinks.styles'
+import React, { useEffect, useState } from "react";
+
+//api fetch service
+import { SearchDrink } from "../../services/drinkServices";
+
+//page components
+import Button from "../../components/Button/Button";
+import NavBar from "../../components/NavBar/NavBar";
+import BackButton from "../../components/Button/BackButton";
+import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
+import CustomCarousel from "../../components/CustomCarousel/CustomCarousel";
+import DrinkCard from "../../components/DrinkCard/DrinkCard";
+
+//style
+import { Container, SearchForm } from "./SearchDrinks.styles";
 
 function SearchDrinks() {
-    return (
-        <Container>
-            
-        </Container>
-    )
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [drinks, setDrinks] = useState([]);
+
+  useEffect(() => {
+    let fetchData = async () => {
+      let apiDrinks = await SearchDrink(search);
+      setDrinks(apiDrinks);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [search, drinks]);
+
+  return (
+    <Container>
+      <NavBar isFilter={true} />
+
+      <BackButton backTo={"menu"} color={"green"} />
+
+      <SearchForm method="GET">
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder={"Type a cocktail name"}
+        />
+        <Button text={"search"} type={"submit"} secondary={true} />
+      </SearchForm>
+
+      <section>
+        {!loading ? (
+          <CustomCarousel>
+            {drinks.map((drink) => (
+              <DrinkCard
+                key={drink.id}
+                id={drink.id}
+                title={drink.name}
+                photo={drink.thumbnail}
+              />
+            ))}
+          </CustomCarousel>
+        ) : (
+          <LoadingSpinner />
+        )}
+      </section>
+    </Container>
+  );
 }
 
-export default SearchDrinks
+export default SearchDrinks;
--- src/pages/SearchDrinks/SearchDrinks.styles.js
+++ src/pages/SearchDrinks/SearchDrinks.styles.js
@@ -1,7 +1,53 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
-    grid-area: main;
-    align-self: center;
+  grid-area: main;
+  width: 90%;
+  justify-self: center;
+  margin: 1rem 0 3rem 0;
+
+  display: grid;
+  grid-template-areas:
+    "nav nav nav"
+    "button form form"
+    "cards cards cards";
+  grid-template-rows: 0.5fr 1fr 3fr;
+  align-items: center;
+  grid-gap: 1rem;
+
+  nav {
+    width: 100%;
+    grid-area: nav;
     justify-self: center;
-`
+    align-self: center;
+    display: flex;
+    justify-content: center;
+  }
+
+  section {
+    grid-area: cards;
+    width: 900px;
+    justify-self: center;
+  }
+
+  a {
+    align-self: center;
+  }
+
+`;
+
+export const SearchForm = styled.form`
+  grid-area: form;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  input {
+    background: none;
+    padding: 0.5rem;
+    width: 40%;
+    border: none;
+    border-bottom: 3px solid var(--green);
+    border-left: 3px solid var(--green);
+  }
+`;
