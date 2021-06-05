f3fa879d46a36b717615541a0aa315c8b5548107
anvitrola <vitoriavianathiss@outlook.com>
2021-06-05 00:03:55 -0300

    fixing english....

--- src/pages/Colab/Colab.js
+++ src/pages/Colab/Colab.js
@@ -17,7 +17,7 @@ function Colab() {
         title={"So you have good ideas..."}
         subtitle={"This is an open source project!"}
         text={
-          "If you are familiar with web development and want to make a contribution with this project, feel free to access it's repository clicking in the image aside and open a pull request (just tag me as a reviewer). If you're not from the area but still have any good idea you can just get in touch with me via LinkedIn - in this case just click in my nickname in the footer!"
+          "If you are familiar with web development and want to make a contribution in this project, feel free to access it's repository by clicking in the image aside and opening a pull request (just tag me as a reviewer). If you're not from the area but still have any good idea you can just get in touch with me via LinkedIn - in this case just click in my nickname in the footer!"
         }
       />
 
